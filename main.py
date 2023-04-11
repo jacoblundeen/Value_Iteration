f3e9f69063bfbaabbda09ed048c290c09e233bdc
Jacob M. Lundeen <jacob.lundeen@gmail.com>
2023-04-11 10:14:11 -0400

    Working on value_iteration() function.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 from typing import List, Tuple, Dict, Callable
+from copy import deepcopy
 
 
 def read_world(filename: str) -> List[List]:
@@ -12,17 +13,39 @@ def read_world(filename: str) -> List[List]:
 
 
 def value_iteration(world: List[List], costs: Dict, goal: Tuple, rewards: int, actions: List, gamma: float) -> Dict:
-    v_s = np.zeros(goal)
+    rows = len(world)
+    cols = len(world[0])
+    v_s = np.zeros((rows, cols))
     t = 0
-    q_left = []
-    q_right = []
-    q_up = []
-    q_down = []
+    q = np.zeros((len(actions), rows, cols))
+    epsilon = 1
+    # q_left = np.zeros(len(goal[0]-1), len(goal))
+    # q_right = np.zeros(len(goal[0]-1), len(goal))
+    # q_up = np.zeros(len(goal[0]-1), len(goal))
+    # q_down = np.zeros(len(goal[0]-1), len(goal))
+    policy = {}
     while epsilon > 0.01:
-        for state in world:
-            for act in actions:
-
-    pass
+        v_last = deepcopy(v_s)
+        t += 1
+        for x in range(rows):
+            for y in range(cols):
+                if (x, y) == goal: reward = 100
+                else: reward = 0
+                for a in range(len(actions)):
+                    if a == 0:
+                        temp = costs[world[x][y]]
+                        q[a,x,y] = reward + gamma * (0.7 * costs[world[x][(y-1)]] + 0.1*costs[world[(x+1)][y]] + 0.1*costs[world[x][(y+1)]] + 0.1*costs[world[(x-1)][y]])
+                    elif a == 1:
+                        q[a,x,y] = reward + gamma * (0.7 * costs[world[x+1][y]] + 0.1*costs[world[x][y+1]] + 0.1*costs[world[x-1][y]] + 0.1*costs[world[x][y-1]])
+                    elif a == 2:
+                        q[a,x,y] = reward + gamma * (0.7 * costs[world[x][y+1]] + 0.1*costs[world[x-1][y]] + 0.1*costs[world[x][y-1]] + 0.1*costs[world[x+1][y]])
+                    else:
+                        q[a,x,y] = reward + gamma * (0.7 * costs[world[x-1][y]] + 0.1*costs[world[x][y-1]] + 0.1*costs[world[x+1][y]] + 0.1*costs[world[x][y+1]])
+                direction = q.argmax(2)
+                policy.update({(x,y): '<'})
+                v_s[x,y] = q[:][x][y].max()
+        epsilon = abs(v_s - v_last)
+    return policy
 
 
 if __name__ == "__main__":
@@ -33,3 +56,6 @@ if __name__ == "__main__":
     cardinal_moves = [(0, -1), (1, 0), (0, 1), (-1, 0)]
     reward = 100
     goal = (len(small_world[0]) - 1, len(small_world) - 1)
+
+    small_policy = value_iteration(small_world, costs, goal, reward, cardinal_moves, gamma)
+    print(small_policy)
